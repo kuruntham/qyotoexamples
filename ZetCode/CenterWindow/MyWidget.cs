2f73a43f85d8572fcf9df2df97e90f9b18baa384
manikk <manikk.h@gmail.com>
2014-07-04 23:29:12 +0530

    added initial zetcode examples

---
+++ ZetCode/CenterWindow/MyWidget.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using QtCore;
+using QtGui;
+
+namespace CenterWindow
+{
+	public class MyWidget : QWidget
+	{
+		const int Wdth = 250;
+		const int Hght = 150;
+
+		public MyWidget ()
+		{
+			WindowTitle = "Centering the window";
+
+			Resize (Wdth, Hght);
+
+			Center ();
+
+			Show ();
+		}
+
+		private void Center() 
+		{
+			QDesktopWidget qdw = new QDesktopWidget ();
+
+			int screenWidth = qdw.Width;
+			int screenHeight= qdw.Height;
+
+			int centerX = (screenWidth - Wdth) / 2;
+			int centerY = (screenHeight - Hght) / 2;
+
+			Move (centerX, centerY);
+		}
+
+		[STAThread]
+		public static int Main(string[] args) 
+		{
+			new QApplication (args);
+			new MyWidget ();
+			return QApplication.Exec ();
+		}
+	}
+}
+
---
+++ ZetCode/Tooltip/MyWidget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using QtCore;
+using QtGui;
+
+namespace Tooltip
+{
+	public class MyWidget : QWidget
+	{
+		public MyWidget ()
+		{
+			WindowTitle = "Tooltip";
+			ToolTip = "This is QWidget";
+			Resize (250, 150);
+			Move (300, 300);
+			Show ();
+		}
+
+		[STAThread]
+		public static int Main(string[] args)
+		{
+			new QApplication (args);
+			new MyWidget ();
+			return QApplication.Exec ();
+		}
+	}
+}
+
