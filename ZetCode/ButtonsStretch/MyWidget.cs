2255a742f0b30284f0feb0d88a408d5c90d704ac
manikk <manikk.h@gmail.com>
2014-07-05 10:28:28 +0530

    added zetcode button example

---
+++ ZetCode/ButtonsStretch/MyWidget.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using QtCore;
+using QtGui;
+
+namespace ButtonsStretch
+{
+    class MyWidget : QWidget
+    {
+        public MyWidget()
+        {
+            WindowTitle = "Button Stretch Example";
+            InitiUI();
+            Resize(300, 200);
+            Move(300, 300);
+            Show();
+        }
+
+        private void InitiUI()
+        {
+            QPushButton okBtn = new QPushButton("Ok", this);
+            QPushButton cancelBtn = new QPushButton("Cancel", this);
+
+            QVBoxLayout vBoxLayout = new QVBoxLayout(this);
+            QHBoxLayout hBoxLayout = new QHBoxLayout();
+
+            hBoxLayout.AddStretch(1);
+            hBoxLayout.AddWidget(okBtn);            
+            hBoxLayout.AddWidget(cancelBtn);
+
+            vBoxLayout.AddStretch(1);
+            vBoxLayout.AddLayout(hBoxLayout);
+            
+        }
+
+        public static int Main(string[] args)
+        {
+            new QApplication(args);
+            new MyWidget();
+            return QApplication.Exec();
+        }
+    }
+}
